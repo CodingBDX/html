af51b38dffe7e152c22c3a712ed9e721f26ca795
CodingBDX <antonycharier@gmail.com>
2022-03-25 14:57:02 +0100

    liquid effect

---
+++ prototype/liquid effect/script.js
@@ -0,0 +1,12 @@
+
+ new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+  image2: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW9kZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+  displacementImage: 'diss.png',
+  
+  
+})
+
+
